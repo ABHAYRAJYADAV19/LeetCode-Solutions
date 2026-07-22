a531de1d69b73bd909ad20c28b7bc60712372d42
Abhay Raj Yadav <165184042+ABHAYRAJYADAV19@users.noreply.github.com>
2026-07-22 11:57:29 +0530

    Time: 1 ms (100%), Space: 46.8 MB (12.86%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,56 +14,29 @@
  * }
  */
 class Solution {
-
-    private TreeNode first;
-    private TreeNode middle;
-    private TreeNode last;
-    private TreeNode prev;
-
-    private void inorder(TreeNode root) {
-        if (root == null)
-            return;
-
-        inorder(root.left);
-
-        if (prev != null && root.val < prev.val) {
-
-            // First violation
-            if (first == null) {
-                first = prev;
-                middle = root;
-            }
-            // Second violation
-            else {
-                last = root;
-            }
-        }
-
-        prev = root;
-
-        inorder(root.right);
-    }
-
+    TreeNode first=null;
+    TreeNode second=null;
+    TreeNode prev=new TreeNode(Integer.MIN_VALUE);
     public void recoverTree(TreeNode root) {
-
-        first = null;
-        middle = null;
-        last = null;
-        prev = new TreeNode(Integer.MIN_VALUE);
-
         inorder(root);
+        int temp=first.val;
+        first.val=second.val;
+        second.val=temp;
+        
+    }
+    private void inorder(TreeNode root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        if(prev.val>root.val){
+            if(first==null){
+                first=prev;
 
-        
-        if (first != null && last != null) {
-            int temp = first.val;
-            first.val = last.val;
-            last.val = temp;
+            }
+            second=root;
         }
-        
-        else if (first != null && middle != null) {
-            int temp = first.val;
-            first.val = middle.val;
-            middle.val = temp;
-        }
+        prev=root;
+        inorder(root.right);
     }
 }
